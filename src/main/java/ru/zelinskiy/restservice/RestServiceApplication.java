c8c6f01bcb93133391e863c172357d8315771181
zelinskiyrk <zelinskiyrk@gmail.com>
2024-04-21 20:21:59 +0300

    first commit

---
+++ src/main/java/ru/zelinskiy/restservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.zelinskiy.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
